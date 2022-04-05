9d84c952b34a78b1f73e48fdf620b1ffd7bc01e2
Alex <alrodriguez41@poligran.edu.co>
2022-04-05 07:09:50 -0500

    Creando nuestra primer entrega

---
+++ app-domainSem15/src/main/java/ar/com/entregas/semana15/carrousel/Carrousel.java
@@ -0,0 +1,58 @@
+package ar.com.entregas.semana15.carrousel;
+
+public class Carrousel {
+	
+	private Long id;
+	private String descripcion;
+	private String imagenpath;
+	private Long activo;
+	
+	public Carrousel(Long id, String descripcion, String imagenpath, Long activo) {
+		this.id = id;
+		this.descripcion = descripcion;
+		this.imagenpath = imagenpath;
+		this.activo = activo;
+	}
+
+	public Carrousel() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getImagenpath() {
+		return imagenpath;
+	}
+
+	public void setImagenpath(String imagenpath) {
+		this.imagenpath = imagenpath;
+	}
+
+	public Long getActivo() {
+		return activo;
+	}
+
+	public void setActivo(Long activo) {
+		this.activo = activo;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ app-webSem15/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>
+		Semana 15
+	</title>
+</head>
+<body>
+	<h1>
+		Semana 15
+	</h1>
+</body>
+</html>
